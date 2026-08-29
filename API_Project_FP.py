fcbf00a3208714d33d293481564c1aac4fa1455c
Mahmoud Osama <mahmoud.osama@egypt.com>
2026-08-29 18:01:38 +0300

    adding project to my GITHUB

--- API_Project_FP.py
+++ API_Project_FP.py
@@ -1,22 +1,25 @@
 # 1. Import libraries
 import requests
 import pyodbc
+import logging
 from datetime import datetime
+import os
+from dotenv import load_dotenv
 
 
 # 2. Getting the Data:
-API_KEY = "Enter_your_weatherstack_API"
-My_DRIVER =  "Enter_your_Driver"
-My_SERVER = r"Enter_your_Server"
-My_DATABASE = "Enter_your_database"
+API_KEY = os.environ.get("API_KEY")
+My_DRIVER = os.environ.get("My_DRIVER")
+My_SERVER = os.environ.get("My_SERVER")
+My_DATABASE = os.environ.get("My_DATABASE")
+User_Agent = os.environ.get("My_User_Agent")
 
-
+logging.basicConfig(filename="API_log.log",level=logging.INFO)
 url_1 = f"http://api.weatherstack.com/current?access_key={API_KEY}&query=Cairo"
-User_1 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36 Edg/149.0.0.0"
-report = requests.get(url = url_1, headers = {"User-Agent":User_1})
+report = requests.get(url = url_1, headers = {"User-Agent":User_Agent})
 data = report.json()
 conn_str = (
-    fr'DRIVER={{My_DRIVER}};'
+    fr'DRIVER={{{My_DRIVER}}};'
     fr'SERVER={My_SERVER};'
     fr'DATABASE={My_DATABASE};'
     r'Trusted_Connection=yes;'
@@ -36,12 +39,13 @@ city, country, region, temperature, date, observation_time = [data['location']['
 objects_time = [data['current']['astro']['sunrise'],
                 data['current']['astro']['sunset'],
                 data['current']['astro']['moonrise'],
-                data['current']['astro']['moonset']]
+                data['current']['astro']['moonset'],
+                data['location']['localtime'][:-6]]
 
 time_format = '%I:%M %p'
 objects_time_formated = []
 
-for i in objects_time:
+for i in objects_time[:-1]:
     try:
         objects_time_formated.append(datetime.strptime(i, time_format))
     except ValueError as e:
@@ -58,6 +62,7 @@ for i in objects_time_formated:
         objects_str_formated.append('NULL')
         print(type(e))
     continue
+objects_str_formated.append(objects_time[-1])
 
 # 4. Creating the tables:
 cursor.execute(""" IF OBJECT_ID('DAILY_Egypt_WEATHER') IS NULL CREATE TABLE DAILY_EGYPT_WEATHER(
@@ -65,48 +70,52 @@ ID INT IDENTITY CONSTRAINT PK_WEATHER_ID PRIMARY KEY,
 CITY NVARCHAR(25),
 COUNTRY NVARCHAR(25),
 REGION NVARCHAR(25),
-TEMPRATURE INT,
-DATE DATE,
+TEMPERATURE INT,
+DATE DATE CONSTRAINT UQ_ID_Date_Weather UNIQUE,
 [OBSERVATION TIME] TIME,
 [WEATHER CONDITION] NVARCHAR(25)
 )
 """)
 cursor.commit()
 
+
 cursor.execute("""IF OBJECT_ID('DAILY_EGYPT_OBJECT_TIME') IS NULL CREATE TABLE DAILY_EGYPT_OBJECT_TIME(
 ID INT CONSTRAINT UQ_ID_OBJECT_TIME UNIQUE ,
 SUNRISE TIME,
 SUNSET TIME,
 MOONRISE TIME,
-MOONSET TIME
+MOONSET TIME,
+Date Date CONSTRAINT UQ_DATE_OBJECT UNIQUE
 )
 """)
 cursor.commit()
 
 
 # 5. Importing Data:
-cursor.execute("""INSERT INTO DAILY_EGYPT_WEATHER(CITY,COUNTRY,REGION,TEMPRATURE,DATE,[OBSERVATION TIME]) 
+cursor.execute("""INSERT INTO DAILY_EGYPT_WEATHER(CITY,COUNTRY,REGION,TEMPERATURE,DATE,[OBSERVATION TIME]) 
                    VALUES(?,?,?,?,?,?)""",
                [city,country,region,temperature,date,observation_time])
 cursor.commit()
 cursor.execute("""
 UPDATE DAILY_EGYPT_WEATHER 
 SET [WEATHER CONDITION] = CASE 
-WHEN TEMPRATURE <= 10 THEN 'COLD' 
-WHEN TEMPRATURE >10 AND TEMPRATURE <=30 THEN 'GOOD' 
-WHEN TEMPRATURE >30 AND TEMPRATURE <40 THEN 'HOT'
-WHEN TEMPRATURE >40 THEN 'VERY HOT'
+WHEN TEMPERATURE <= 10 THEN 'COLD' 
+WHEN TEMPERATURE >10 AND TEMPERATURE <=30 THEN 'GOOD' 
+WHEN TEMPERATURE >30 AND TEMPERATURE <40 THEN 'HOT'
+WHEN TEMPERATURE >40 THEN 'VERY HOT'
 END
 """)
 cursor.commit()
-
+logging.info(f"The insertion date is {datetime.now()} for DAILY_Egypt_WEATHER ")
 
 cursor.execute("""
 INSERT INTO DAILY_EGYPT_OBJECT_TIME(ID,SUNRISE,
 SUNSET,
 MOONRISE,
-MOONSET) 
-VALUES(@@IDENTITY,?,?,?,?)
+MOONSET,
+Date) 
+VALUES(@@IDENTITY,?,?,?,?,?)
 """
 ,objects_str_formated)
 cursor.commit()
+logging.info(f"The insertion date is {datetime.now()} for DAILY_EGYPT_OBJECT_TIME")
